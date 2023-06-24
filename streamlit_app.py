06bdb5c35571af9e721699b554085340de440649
Barayalon <137609315+Barayalon@users.noreply.github.com>
2023-06-24 13:47:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,14 @@
 import streamlit as st
 #from dotenv import load_dotenv
 
-
 def main():
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
 
-
-
-st.header("Chat with multiple PDFs :books:")
-user_question = st.text_input("Ask a question about your documents:")
-
-with st.sidebar:
-    st.subheader("Your documents")
-    pdf_docs = st.file_uploader(
-        "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
                
 
 if __name__ == '__main__':
