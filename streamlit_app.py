a14f90b149395aafcf38f6c9a6f5bd650233a1b9
Barayalon <137609315+Barayalon@users.noreply.github.com>
2023-06-24 20:36:11 +0300

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -204,7 +204,7 @@ def main():
 
     user_question = st.text_input("are you ready to start?")
     question_num = st.session_state.question_num
-    if st.button("Next Question"+str(st.session_state.question_num) ): 
+    if st.button("Next Question "+str(question_num) ): 
       next_question = 1 + int(question_num)
       st.session_state.question_num = next_question 
 
