aa1a37b5538b4ecf8176cde98f3b59d3cf3a4c21
Barayalon <137609315+Barayalon@users.noreply.github.com>
2023-06-24 15:54:39 +0300

    updated functions and requerements

---
+++ prompt.py
@@ -0,0 +1,103 @@
+example_questions = """
+
+Assay questions:
+The following questions are taken from an assay about ALASKA'S MELTING GLACIERS.
+
+ALASKA'S MELTING GLACIERS
+
+I Most people know about the impressive glaciers of Alaska, even if they've never been there.
+Glaciers are enormous masses of ice formed by the accumulation of snow on mountains over
+thousands of years. However, visitors expecting to see the wonders of glaciers may find only bare
+mountainsides in some places because all the ice has melted and the water has flowed into the
+oceans. Unfortunately, climate change is causing these spectacular glaciers to melt and gradually
+disappear. This is more than a loss of beauty: the glaciers of the world have kept the Earth cool and
+the sea level constant for thousands of years.
+II Scientists are trying to find out how fast the glaciers in Alaska are melting and how much
+water is flowing from them into the oceans. One recent study has discovered that climate change
+has caused the glaciers to melt much faster than had been previously predicted. The result is that
+enormous amounts of water are flowing into the oceans. Another study, using photographs taken
+from satellites, predicts that these glaciers will shrink to about half their current size over the next
+few years, a much more rapid decrease than expected.
+III It is not only the glaciers in Alaska that are disappearing: glaciers are melting all over the
+world. Researchers believe that in the decades ahead the increasingly rapid melting of the world's
+glaciers will cause the sea level to rise by about 25 centimeters. This rise is one of the most serious
+results of climate change and it will have an enormous impact on society. Large areas of farmland
+will disappear under the rising oceans, causing food shortages. Also, the flooding of large coastal
+population centers around the globe, like New York City and Bangkok, may result in the migration
+of millions of people. The consequences of these developments may be widespread starvation and
+disease. Some scientists believe that Alaska in particular may play a critical role in the rise of the
+sea level worldwide. This is because many of Alaska's glaciers are near the ocean, where they melt
+even more quickly.
+IV Volunteers at the Mendenhall Glacier Visitor Center in Juneau, Alaska, use displays, lectures
+and videos to teach about the dangerous effects of climate change. "People are shocked when
+they see a video showing one glacier getting smaller and smaller over time," says John Shane, a
+volunteer at the Visitor Center. "They also learn that the glaciers are disappearing so quickly mainly
+because of human-caused climate change. I hope everyone realizes that the future of our planet
+mostly depends on our actions today."
+
+
+Multiple choices question:
+1. PUT A √ BY THE TWO CORRECT ANSWERS.
+What do we learn in paragraph I?
+...... i) How climate change affects the glaciers of Alaska.
+...... ii) How people learn about glaciers.
+....... iii) What has caused climate change.
+...... iv) How glaciers were created.
+...... v) Why most people have never seen glaciers.
+...... vi) Where most of the world's glaciers can be found
+(2x6=12 points)
+
+Open question:
+
+2. What may happen if glaciers continue to melt? Give ONE answer. (paragraph I)
+ANSWER: ................................................................................................................................................ .
+(7 points)
+
+
+Multiple choice 1 correct answer:
+3. What do the two studies described in paragraph II have in common?
+They both (–).
+i) base their predictions on satellite photographs of the glaciers
+ii) measure the amount of water flowing from the glaciers into the oceans
+iii) research the effect of glaciers on the world's climate
+iv) focus on the disappearance of Alaska's glaciers
+(6 points)
+
+Complete the missing information:
+
+4. COMPLETE THE MISSING INFORMATION ACCORDING TO PARAGRAPH III:
+(1) Scientists predict that melting glaciers will .................................................................................. .
+(2) This will cause the flooding of cities.
+(3) As a result, .................................................................................................................................... .
+(4) This will lead to a disaster.
+(2x7=14 points)
+
+2 Open questions:
+5. The disasters that are described in lines 17-20 can have TWO terrible consequences. What are these
+TWO consequences? (paragraph III)
+ANSWER: .............................................................................................................................................. .
+(8 points)
+
+6. Why do some scientists believe that the location of Alaska's glaciers near the ocean is significant?
+Take your answer from paragraph III.
+ANSWER: .............................................................................................................................................. .
+(7 points)
+
+Multiple choices:
+7. In paragraph IV we are told about (–).
+i) the future of the Glacier Visitor Center in Juneau, Alaska
+ii) the various ways visitors learn about glaciers
+iii) the actions we can take to prevent climate change
+iv) the dangers of visiting the Alaskan glaciers
+(6 points)
+
+Part II written production:
+Write 100-120 words in English on the following topic.
+8. Although Israel has no glaciers, it attracts many tourists. What attractions would you recommend
+visiting in Israel and why?
+(40 points)
+
+
+
+
+"""
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,12 @@ __version__ = "0.0.0.1"
 app_name = "Build my exam"
 
 import streamlit as st
-from dotenv import load_dotenv
 import os
+from PyPDF2 import PdfReader
+import openai
+import functions
+import prompt
+
 
 st.set_page_config(layout='centered', page_title=f'{app_name} {__version__}')
 ss = st.session_state
@@ -14,9 +18,16 @@ def on_api_key_change():
 	api_key = ss.get('api_key') or os.getenv('OPENAI_API_KEY')
 	os.environ['OPENAI_API_KEY'] = api_key
 
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 
-#from dotenv import load_dotenv
+
 
 
 
@@ -24,7 +35,7 @@ def main():
     st.write('## 1. Enter your OpenAI API key')
     st.text_input('OpenAI API key', type='password', key='api_key', on_change=on_api_key_change, label_visibility="collapsed")
     st.write(os.environ['OPENAI_API_KEY'])
-    load_dotenv()
+    
     
     st.header("Chat with multiple PDFs :books:")
     user_question = st.text_input("Ask a question about your documents:")
@@ -33,7 +44,13 @@ def main():
         st.subheader("Your documents")
         pdf_docs = st.file_uploader(
             "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-               
+        if st.button("Generate Exam"):
+             with st.spinner("Processing"):
+                  # get pdf text
+                  raw_text = get_pdf_text(pdf_docs)
+                  # new exam
+                  
+
 
 if __name__ == '__main__':
     main()
