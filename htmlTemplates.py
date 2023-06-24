837d0e85fdfce209fd5ee1574406003b679cf68a
Barayalon <137609315+Barayalon@users.noreply.github.com>
2023-06-24 23:34:44 +0300

    update

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/WVGHHmM/png-transparent-graphy-student-students-class-photography-people-thumbnail.png">
+        <img src="https://i.ibb.co/2ZWFFGC/png-transparent-graphy-student-students-class-photography-people-thumbnail.png">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
