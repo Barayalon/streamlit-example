ac503c4aa346e97514a460c52a7dfe32f58c77d4
Barayalon <137609315+Barayalon@users.noreply.github.com>
2023-07-03 12:46:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -188,10 +188,10 @@ def check_answers(question, answer, sources):
 def main():
     st.header("Class AI",)
     st.session_state.question_num = 0
-    if os.environ['OPENAI_API_KEY'] == '123123':
-      st.write('## 1. Enter your OpenAI API key')
-      st.text_input('OpenAI API key', type='password', key='api_key', on_change=on_api_key_change, label_visibility="collapsed")
-      st.write(os.environ['OPENAI_API_KEY'])
+    
+	st.write('## 1. Enter your OpenAI API key')
+	st.text_input('OpenAI API key', type='password', key='api_key', on_change=on_api_key_change, label_visibility="collapsed")
+	st.write(os.environ['OPENAI_API_KEY'])
     
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
