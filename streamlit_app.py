138e6859bf6dede01f7f6921efdc242cd29239ef
Barayalon <137609315+Barayalon@users.noreply.github.com>
2023-07-03 12:48:57 +0300

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -189,7 +189,7 @@ def main():
     st.header("Class AI",)
     st.session_state.question_num = 0
     
-	  st.write('## 1. Enter your OpenAI API key')
+	st.write('## 1. Enter your OpenAI API key')
 	  st.text_input('OpenAI API key', type='password', key='api_key', on_change=on_api_key_change, label_visibility="collapsed")
 	  st.write(os.environ['OPENAI_API_KEY'])
     
