6028615162e68c01798862890bc891425bce1189
Barayalon <137609315+Barayalon@users.noreply.github.com>
2023-07-09 09:31:37 +0300

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,10 +42,10 @@ def handle_answer(question):
       for i, message in enumerate(history):
           message = message.replace("\n","<br/>")
           if i % 2 == 0:
-              st.write(bot_template.replace(
+              st.write(uesr_template.replace(
                   "{{MSG}}", message), unsafe_allow_html=True)
           else:
-              st.write(user_template.replace(
+              st.write(bot_template.replace(
                   "{{MSG}}", message), unsafe_allow_html=True)
     else:
       st.write(bot_template.replace(
